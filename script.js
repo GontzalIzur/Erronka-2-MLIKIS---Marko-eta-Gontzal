63cb755e017daa24872c1e0924e3de135c7f0e46
GontzalIzur <gontzalizurza@gmail.com>
2022-05-16 13:39:56 +0200

    ---

--- script.js
+++ script.js
@@ -1,44 +1,37 @@
-
 function f1() {
-       let a = document.getElementById("formu").value;
-        console.log(a);
-       if (a == "Lan") {
-        document.getElementById("divfirst").style.display="flex";
-        document.getElementById("divsecond").style.display="none";
-        document.getElementById("divthird").style.display="none";
-        document.getElementById("divfourth").style.display="none";
-       }
-       else if (a=="Erosi"){
-        document.getElementById("divsecond").style.display="flex";
-        document.getElementById("divfirst").style.display="none";
-        document.getElementById("divthird").style.display="none";
-        document.getElementById("divfourth").style.display="none";
+  let a = document.getElementById("formu").value;
+  console.log(a);
+  if (a == "Lan") {
+    document.getElementById("divfirst").style.display = "flex";
+    document.getElementById("divsecond").style.display = "none";
+    document.getElementById("divthird").style.display = "none";
+    document.getElementById("divfourth").style.display = "none";
+  } else if (a == "Erosi") {
+    document.getElementById("divsecond").style.display = "flex";
+    document.getElementById("divfirst").style.display = "none";
+    document.getElementById("divthird").style.display = "none";
+    document.getElementById("divfourth").style.display = "none";
+  } else if (a == "Matxura") {
+    document.getElementById("divthird").style.display = "flex";
+    document.getElementById("divsecond").style.display = "none";
+    document.getElementById("divfirst").style.display = "none";
+    document.getElementById("divfourth").style.display = "none";
+  } else if (a == "Ordutegiak") {
+    document.getElementById("divfourth").style.display = "flex";
+    document.getElementById("divsecond").style.display = "none";
+    document.getElementById("divfirst").style.display = "none";
+    document.getElementById("divthird").style.display = "none";
+  }
+}
 
-       }
-       else if (a=="Matxura"){
-        document.getElementById("divthird").style.display="flex";
-        document.getElementById("divsecond").style.display="none";
-        document.getElementById("divfirst").style.display="none";
-        document.getElementById("divfourth").style.display="none";
-
-       }
-       else if (a=="Ordutegiak"){
-        document.getElementById("divfourth").style.display="flex";
-        document.getElementById("divsecond").style.display="none";
-        document.getElementById("divfirst").style.display="none";
-        document.getElementById("divthird").style.display="none";
-
-    
-
-       }
+function f2(){
        
 }
 
 function ASCII63() {
-
-
-document.getElementById("hihi").style.display="inline";
-var audio = new Audio("erronka1/MULTIMEDIA/ooo popeyes biscuits AHH echoed Sound Effect.mp3");
-audio.play();
-
+  document.getElementById("hihi").style.display = "inline";
+  var audio = new Audio(
+    "erronka1/MULTIMEDIA/ooo popeyes biscuits AHH echoed Sound Effect.mp3"
+  );
+  audio.play();
 }
