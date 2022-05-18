8c4d057505dd0467974ab3e45f9430a731d77fa9
ByMarko99 <markogrosal@elorrieta-errekamari.com>
2022-05-18 14:24:14 +0200

    aa

--- script.js
+++ script.js
@@ -24,8 +24,8 @@ function formularioaErakutsi() {
   }
 }
 
-function TextAreaErakutsi(){
-       
+function TextAreaErakutsi() {
+
 }
 
 function ASCII63() {
@@ -33,3 +33,78 @@ function ASCII63() {
   var audio = new Audio("erronka1/MULTIMEDIA/augh.mp3");
   audio.play();
 }
+
+let datuak = [];
+
+function gorde() {
+  
+  let sexo = "";
+    let data = document.getElementById("date").value;
+    let izena = document.getElementById("name").value;
+    let izena2 = document.getElementById("name2").value;
+    let emaila = document.getElementById("email").value;
+    let telefono = document.getElementById("phone").value;
+    if (document.querySelector('input[name="galdera"]:checked').checked) {
+       sexo = document.querySelector('input[name="galdera"]:checked').value;
+    }else{
+       sexo = "EZ"
+    }
+   
+    let helbidea = document.getElementById("helbidea").value;
+    let helbidea2 = document.getElementById("helbidea2").value;
+    let helbidea3 = document.getElementById("helbidea3").value;
+    let paisuwu = document.getElementById("aa").options[document.getElementById("aa").selectedIndex].text;
+    let gaixotasun = document.getElementById("gaixo").value;
+    let textarea = document.getElementById("komentarioa").value;
+    let cv = "EZ";
+    let balorazioa = document.querySelector('input[name="stars"]:checked').value;
+    
+  
+    if (document.getElementById("CV").value != "") {
+      cv = "BAI";
+    }
+  
+    if (document.getElementById("gaixo").value == "") {
+      gaixotasun = "EZ";
+    }
+  
+    if (document.getElementById("komentarioa").value == "") {
+      textarea = "EZ";
+    }
+  
+    if (document.querySelector('input[name="stars"]:checked').length <= 0) {
+      balorazioa = "EZ";
+    }
+  
+  
+  
+    let ob1 = {
+      data: data,
+      izena: izena,
+      izena2: izena2,
+      emaila: emaila,
+      telefono: telefono,
+      sexo: sexo,
+      helbidea: helbidea,
+      helbidea2: helbidea2,
+      helbidea3: helbidea3,
+      paisuwu: paisuwu,
+      gaixotasun: gaixotasun,
+      textarea: textarea,
+      cv: cv,
+      balorazioa: balorazioa,
+    }
+  
+    datuak.push(ob1);
+  
+    // Textareako datuen goiburua idatzi
+  
+    document.getElementById("tiketa").value = "Data\tIzena\tAbizena\tEmaila\tTelefonoa\tSexua\tHelbidea\tHerrialdea\tGaixotasunak\tKomentarioak\tCurriculum\tBalorazioa";
+  
+    document.getElementById("tiketa").value += "\n" + ob1.data + "\t" + "\t" + ob1.izena + "\t" + "\t" + ob1.izena2 + "\t" + "\t" + ob1.emaila + "\t" + "\t" + ob1.telefono + "\t" + "\t" + ob1.sexo + "\t" + "\t" + ob1.helbidea2 + "\t" + "\t" + ob1.helbidea3 + "\t" + "\t" + ob1.paisuwu + "\t" + "\t" + ob1.gaixotasun + "\t" + "\t" + ob1.textarea + "\t" + "\t" + ob1.cv + "\t" + "\t" + ob1.balorazioa;
+  
+  }
+
+ 
+
+
